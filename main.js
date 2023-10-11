6ebaff9471d369223e20ca7e1f86bb6bf1e3ef36
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-10-11 14:50:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 //https://teachablemachine.withgoogle.com/models/[...]//
+var img
+Webcam.set({
+   width:350,
+   height:300,
+   image_format:"png",
+   png_quality:90 
+});
+
+var camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function takeSnapshot() {
+    Webcam.snap(function(datauri){
+        document.getElementById("result").innerHTML="<img id='foto' src='"+datauri+"'>";
+    });
+}
+
+console.log("versão ml5:", ml5.version);
+
+var classificadora=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/model.json", modelLoaded);
